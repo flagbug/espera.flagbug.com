a9340f83d612b60f9ea56c9efb86aff690c5b2e0
Dennis Daume <daume.dennis@gmail.com>
2014-04-09 23:33:38 +0000

    Don't link directly

--- app.rb
+++ app.rb
@@ -10,6 +10,14 @@ get '/EsperaSetup.exe' do
   redirect 'http://espera.s3.amazonaws.com/EsperaSetup.exe'
 end
 
+get '/EsperaPortable.zip' do
+  redirect 'http://espera.s3.amazonaws.com/EsperaPortable.zip'
+end
+
+get '/EsperaBetaSetup.exe' do
+  redirect 'http://espera.s3.amazonaws.com/EsperaBetaSetup.exe'
+end
+
 get '/release-notes' do
   client = HTTPClient.new
   content = client.get_content("https://s3.amazonaws.com/espera/Changelog.md")
