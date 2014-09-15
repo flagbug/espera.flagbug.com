dcc554103bcae1f1ef053cc59bf6e1f5385e327c
Dennis Daume <daume.dennis@gmail.com>
2014-09-15 12:11:13 +0000

    Distribute via Cloudfront

--- app.rb
+++ app.rb
@@ -7,37 +7,24 @@ get '/' do
 end
 
 get '/EsperaSetup.exe' do
-  redirect 'http://espera.s3.amazonaws.com/EsperaSetup.exe'
+  redirect 'http://d1lzv0n76maidw.cloudfront.net//EsperaSetup.exe'
 end
 
 get '/EsperaPortable.zip' do
-  redirect 'http://espera.s3.amazonaws.com/EsperaPortable.zip'
-end
-
-get '/EsperaBetaSetup.exe' do
-  redirect 'http://espera.s3.amazonaws.com/EsperaBetaSetup.exe'
+  redirect 'http://d1lzv0n76maidw.cloudfront.net/EsperaPortable.zip'
 end
 
 get '/Changelog.md' do
   redirect 'http://espera.s3.amazonaws.com/Changelog.md'
 end
 
-get '/Releases/Stable/RELEASES' do
-  redirect 'http://espera.s3.amazonaws.com/Releases/Stable/RELEASES'
-end
-
-get '/Releases/Stable/:name' do
-  raise Sinatra::NotFound unless params[:name].match /Espera-\d+.\d+.\d+-(full|delta).nupkg/
-  redirect 'http://espera.s3.amazonaws.com/Releases/' + params[:name]
-end
-
 get '/releases/clickonce/:name' do
-  redirect 'http://espera.s3.amazonaws.com/releases/clickonce/' + params[:name]
+  redirect 'http://d1lzv0n76maidw.cloudfront.net/releases/clickonce/' + params[:name]
 end
 
 get '/release-notes' do
   client = HTTPClient.new
-  content = client.get_content("http://espera.s3.amazonaws.com/Changelog.md")
+  content = client.get_content("http://d1lzv0n76maidw.cloudfront.net/Changelog.md")
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   @rendered = markdown.render(content)
   
