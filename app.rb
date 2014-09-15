6320581e16ff86651cbc102668783d940202ce63
Dennis Daume <daume.dennis@gmail.com>
2014-09-15 12:34:58 +0000

    Nevermind, CNAME that

--- app.rb
+++ app.rb
@@ -7,24 +7,24 @@ get '/' do
 end
 
 get '/EsperaSetup.exe' do
-  redirect 'http://d1lzv0n76maidw.cloudfront.net/EsperaSetup.exe'
+  redirect 'http://download.getespera.com/EsperaSetup.exe'
 end
 
 get '/EsperaPortable.zip' do
-  redirect 'http://d1lzv0n76maidw.cloudfront.net/EsperaPortable.zip'
+  redirect 'http://download.getespera.com/EsperaPortable.zip'
 end
 
 get '/Changelog.md' do
-  redirect 'http://d1lzv0n76maidw.cloudfront.net/Changelog.md'
+  redirect 'http://download.getespera.com/Changelog.md'
 end
 
 get '/releases/clickonce/:name' do
-  redirect 'http://d1lzv0n76maidw.cloudfront.net/releases/clickonce/' + params[:name]
+  redirect 'http://download.getespera.com/releases/clickonce/' + params[:name]
 end
 
 get '/release-notes' do
   client = HTTPClient.new
-  content = client.get_content("http://d1lzv0n76maidw.cloudfront.net/Changelog.md")
+  content = client.get_content("http://download.getespera.com/Changelog.md")
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   @rendered = markdown.render(content)
   
